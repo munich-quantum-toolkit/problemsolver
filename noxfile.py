9967c2c34c5b691c757c167d5f6422fe74f7ffd7
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-09-23 21:06:48 +0200

    Improve nox sessions

--- noxfile.py
+++ noxfile.py
@@ -11,14 +11,16 @@
 from __future__ import annotations
 
 import argparse
+import contextlib
 import os
 import shutil
+import tempfile
 from typing import TYPE_CHECKING
 
 import nox
 
 if TYPE_CHECKING:
-    from collections.abc import Sequence
+    from collections.abc import Generator, Sequence
 
 
 nox.needs_version = ">=2024.3.2"
@@ -30,6 +32,17 @@ PYTHON_ALL_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
 
 if os.environ.get("CI", None):
     nox.options.error_on_missing_interpreters = True
+
+
+@contextlib.contextmanager
+def preserve_lockfile() -> Generator[None]:
+    """Preserve the lockfile by moving it to a temporary directory."""
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        shutil.move("uv.lock", f"{temp_dir_name}/uv.lock")
+        try:
+            yield
+        finally:
+            shutil.move(f"{temp_dir_name}/uv.lock", "uv.lock")
 
 
 @nox.session(reuse_venv=True)
@@ -79,25 +92,14 @@ def tests(session: nox.Session) -> None:
 @nox.session(reuse_venv=True, venv_backend="uv", python=PYTHON_ALL_VERSIONS)
 def minimums(session: nox.Session) -> None:
     """Test the minimum versions of dependencies."""
-    _run_tests(
-        session,
-        install_args=["--resolution=lowest-direct"],
-        pytest_run_args=["-Wdefault"],
-    )
-    env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
-    session.run("uv", "tree", "--frozen", env=env)
-    session.run("uv", "lock", "--refresh", env=env)
-
-
-@nox.session(reuse_venv=True, venv_backend="uv", python=PYTHON_ALL_VERSIONS)
-def qiskit(session: nox.Session) -> None:
-    """Tests against the latest version of Qiskit."""
-    _run_tests(
-        session,
-        extra_command=["uv", "pip", "install", "qiskit[qasm3-import] @ git+https://github.com/Qiskit/qiskit.git"],
-    )
-    env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
-    session.run("uv", "pip", "show", "qiskit", env=env)
+    with preserve_lockfile():
+        _run_tests(
+            session,
+            install_args=["--resolution=lowest-direct"],
+            pytest_run_args=["-Wdefault"],
+        )
+        env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
+        session.run("uv", "tree", "--frozen", env=env)
 
 
 @nox.session(reuse_venv=True)
@@ -111,17 +113,6 @@ def docs(session: nox.Session) -> None:
     if serve:
         session.install("sphinx-autobuild")
 
-    env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
-    # install build and docs dependencies on top of the existing environment
-    session.run(
-        "uv",
-        "sync",
-        "--inexact",
-        "--only-group",
-        "docs",
-        env=env,
-    )
-
     shared_args = [
         "-n",  # nitpicky mode
         "-T",  # full tracebacks
@@ -131,12 +122,13 @@ def docs(session: nox.Session) -> None:
         *posargs,
     ]
 
+    env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
     session.run(
         "uv",
         "run",
         "--no-dev",  # do not auto-install dev dependencies
-        "--no-build-isolation-package",
-        "mqt-problemsolver",  # build the project without isolation
+        "--group",
+        "docs",
         "sphinx-autobuild" if serve else "sphinx-build",
         *shared_args,
         env=env,
