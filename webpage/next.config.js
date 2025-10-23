250b139b040e6e933df48112bdef50aaa14fce01
Damian Rovara <damianrovara@gmail.com>
2025-10-23 12:51:58 +0200

    ci: :construction_worker: fix basePath for webpage

--- webpage/next.config.js
+++ webpage/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/problemsolver",
+  assetPrefix: "/problemsolver/",
   images: { unoptimized: true },
 };
 
